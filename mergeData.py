2d41596ff5573eb9cf5e9a1f375d861b51dfcc2d
luciasalar <luciasalar@gmail.com>
2018-01-30 14:17:27 +0000

    Add files via upload

--- mergeData.py
+++ mergeData.py
@@ -33,16 +33,8 @@ status_dep_demog_like_big5_schwartz_swl = pd.merge(status_dep_demog_like_big5_sc
 status_dep_demog_like_big5_schwartz_swl = status_dep_demog_like_big5_schwartz_swl.drop_duplicates(subset = 'userid') #333
 
 
-#status_dep_demog.to_csv("status_dep_demog_unique.csv")
+status_dep_demog_like_big5_schwartz_swl.to_csv("status_dep_demog_like_big5_schwartz_swl.csv")
 
-
-
-#like_dic1 = like_dic[:10000000]
-# like_dic2 = like_dic.groupby(['userid'])['like_id']
-
-# status_dep_demoq_like = pd.merge(status_dep_demog,like_dic1, on = 'userid', how='inner')
-
-# status_dep_demoq_like2 = status_dep_demoq_like[pd.notnull(status_dep_demoq_like['text'])]
 
 
 #clean text
@@ -66,6 +58,8 @@ def sanitize(sent):
 
 status_dep_demog_like_big5_schwartz_swl['text'] = status_dep_demog_like_big5_schwartz_swl['text'].apply(sanitize)
 status['text'] = status['text'].apply(sanitize)
+
+
 
 #convert variable to xml
 # def func(row):
@@ -113,7 +107,7 @@ for i in root:
 			
 
 
-tree2 = ET.parse('status_dep_demog_like2.xml')
+tree2 = ET.parse('status_dep_demog_like_big5_schwartz_swl.xml')
 
 tmp = None
 root2 = tree2.getroot()
@@ -168,37 +162,70 @@ for i in range(1, len(status['userid'])):
 
 tree2.write('status_dep_demog_like.xml')
 
-status = status.iloc[1:]
+# convert XML to csv
 
-# reverse_map = {}                                                             
-#      for i in range(len(status['userid'])):                       
-#      	reverse_map[status['userid'][i]] = i
-
-# for i in root2:
-# 	userid = i.attrib['value']
-# 	line_number = reverse_map[userid]
-# 	for j in i:
-# 		if j.attrib['name'] == "text":
-# 			j.text = j.text + " " + status['text'][line_number]
-# 			print(j.text)
+#for i in root2:
+#    first = i.find('value').text
+#    for j in i.find('name'):
+#        second = j.find('name').text
+#        print('{},{}'.format(first, second))
 
 
-
-# [i for i in status['userid']]
-# status['text']
+#for i in root2:
+#	first = i.text
+#	for j in i:
+#		second = j.text
+#		print('{},{}'.format(first, second))
 
 
 
 
 
+status['text'] = status['text'].apply(sanitize)
+
+########group data
+g = status.groupby('userid')['text'].apply(lambda x: "%s" % ', '.join(x))
+g2 = pd.DataFrame(g)
+
+####store data then read it again, data in the correct format
+g2.to_csv("grouped_status.csv")
+g2 = pd.read_csv('grouped_status.csv')
+
+status_dep = pd.merge(g2, dep, on='userid', how = 'inner')
+status_dep = status_dep.drop_duplicates(subset='userid')
+status_dep_demog = pd.merge(status_dep, demog, on='userid')
+status_dep_demog_big5 = pd.merge(status_dep_demog, big5, on='userid', how ='inner') #975
+status_dep_demog_big5_schwartz = pd.merge(status_dep_demog_big5, schwartz, on='userid', how ='inner')  
+status_dep_demog_big5_schwartz = status_dep_demog_big5_schwartz.drop_duplicates(subset = 'userid') #558
+status_dep_demog_big5_schwartz_swl = pd.merge(status_dep_demog_big5_schwartz, swl, on='userid', how ='inner') #395
+status_dep_demog_big5_schwartz_swl = status_dep_demog_big5_schwartz_swl.drop_duplicates(subset = 'userid') #333
 
 
+status_dep_demog_big5_schwartz_swl.to_csv("status_dep_demog_big5_schwartz_swl.csv")
+
+#like dictionary 
+like1 = like_dic[:10000000]
+like2 = like_dic[10000000:20000000]
+like3 = like_dic[20000000:30000000]
+like4 = like_dic[30000000:]
+like_g1 = like1.groupby('userid')['like_id'].apply(lambda x: "%s" % ', '.join(x))
+like_g2 = like2.groupby('userid')['like_id'].apply(lambda x: "%s" % ', '.join(x))
+like_g3 = like3.groupby('userid')['like_id'].apply(lambda x: "%s" % ', '.join(x))
+like_g4 = like4.groupby('userid')['like_id'].apply(lambda x: "%s" % ', '.join(x))
+
+like_g1 = pd.DataFrame(like_g1)
+like_g1.to_csv("like_g1.csv")
+like_g1= pd.read_csv('like_g1.csv')
 
 
+like_g2 = pd.DataFrame(like_g2)
+like_g2.to_csv("like_g2.csv")
+like_g2= pd.read_csv('like_g2.csv')
 
+like_g3 = pd.DataFrame(like_g3)
+like_g3.to_csv("like_g3.csv")
+like_g3= pd.read_csv('like_g3.csv')
 
-
-
-
-
-
+like_g4 = pd.DataFrame(like_g4)
+like_g4.to_csv("like_g4.csv")
+like_g4= pd.read_csv('like_g4.csv')
