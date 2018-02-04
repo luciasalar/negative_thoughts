ade21a435dd4d8caff1adde72cbb168e2d459460
luciasalar <luciasalar@gmail.com>
2018-02-04 20:17:31 +0000

    Add files via upload

---
+++ negative_thoughts.R
@@ -0,0 +1,51 @@
+require(plyr)
+require(stringr)
+require(tidyverse)
+require(stringi)
+require(ICC)
+#result from sentistrengh
+#a1228449 contains the agreement data
+thoughts_agg <- read.csv("a1228449.csv", header = T, fill=TRUE,row.names=NULL)
+thoughts <- read.csv("f1228449.csv", header = T, fill=TRUE,row.names=NULL)
+thoughts %>% select(X_unit_id, negative_yn, thoughtcat, text) -> thought2
+
+#crowdflower agreement
+#Once a job is complete, all of the judgments on a row of data will be aggregated with a confidence score. 
+#The confidence score describes the level of agreement between multiple contributors (weighted by each contributors’ trust scores), and indicates our “confidence” in the validity of the aggregated answer for each row of data. The aggregate result is chosen based on the response with the greatest confidence.
+mean(thoughts_agg$negative_yn.confidence)   
+# 0.8573093
+
+#remove na and 0 
+#filter(thoughts_agg, thoughtcat.confidence != "0" ) -> thought_cat
+
+thoughts_agg$thoughtcat.confidence[is.na(thoughts_agg$thoughtcat.confidence)] <- 0
+mean(thought_cat$thoughtcat.confidence, na.rm = TRUE)
+#0.751999
+
+
+#recode label
+thought2$negative_yn <- revalue(thought2$negative_yn, c("yes"="1", "no"="0", "mixed" = "2"))
+thought2$negative_yn <- as.numeric(thought2$negative_yn)
+thought2$thoughtcat[is.na(thought2$thoughtcat)] <- 0
+
+
+ICCbare(as.factor(thought2$X_unit_id), thought2$thoughtcat, thought2)
+#0.617
+ICCbare(as.factor(thought2$X_unit_id), thought2$negative_yn, thought2)
+#0.55
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
